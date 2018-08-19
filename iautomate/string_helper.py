1aeacdaf55e3023a120f7a70b3fbc8481d15d9b9
Ehsan <iranianpep@gmail.com>
2018-08-19 17:19:47 +1000

    fix travis

--- iautomate/string_helper.py
+++ iautomate/string_helper.py
@@ -1,8 +1,5 @@
-import string
-
-
 class StringHelper(object):
     # run the shell command and print the output if is in debug mode
     @staticmethod
     def replace_placeholder(to_change_string, placeholder, value):
-        return string.replace(to_change_string, placeholder, value)
+        return to_change_string.replace(placeholder, value)
